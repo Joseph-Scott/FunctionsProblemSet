af824770cadb8d81cfd50f1da2abd22d80864a90
Joseph Scott <jbs198585@gmail.com>
2019-06-25 06:17:31 -0400

    boilerplate and problem set

---
+++ functions.js
@@ -0,0 +1,14 @@
+// isEven() - Write a function isEven() which takes a single numberic
+// argument and returns true if the numbers is even, and false otherwise
+
+
+
+
+
+// factorial() - Write a function factorial() which takes a single numeric argument and returns the factorial of that number
+
+
+
+
+
+// kebabToSnake() - Write a function kebabToSnake() which takes a single kebab-cased string argument and returns the snake_cased version.
